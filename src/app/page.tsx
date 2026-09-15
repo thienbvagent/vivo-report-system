2656d9a6f97ada988af47ae0ad3dfcd33097e0d8
Văn Thiện Bùi <thienbv.agent@gmail.com>
2026-09-16 00:17:54 +0700

    fix runtime session page prerender

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 import { getCurrentSession } from '@/lib/auth';
-
+export const dynamic = 'force-dynamic';
 export default async function HomePage() {
   const session = await getCurrentSession();
   if (session) {
